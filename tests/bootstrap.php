e343cebd1e3885e9c67be51f5367b36ac7b528f2
Brad Kent <bkfake-github@yahoo.com>
2018-01-29 22:28:20 -0600

    travis.yml

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,9 @@
 
 // backward compatibility
 $classMap = array(
-    '\PHPUnit_Framework_Exception' => '\PHPUnit\Framework\Exception',
-    '\PHPUnit_Framework_TestCase' => '\PHPUnit\Framework\TestCase',
-    '\PHPUnit_Framework_TestSuite' => '\PHPUnit\Framework\TestSuite',
+    'PHPUnit_Framework_Exception' => 'PHPUnit\Framework\Exception',
+    'PHPUnit_Framework_TestCase' => 'PHPUnit\Framework\TestCase',
+    'PHPUnit_Framework_TestSuite' => 'PHPUnit\Framework\TestSuite',
 );
 foreach ($classMap as $old => $new) {
     if (!class_exists($new)) {
